b09b3448d0bba0a612a404e39e9dfd62f5ad7c14
Jakub <staniojakub@gmail.com>
2020-06-06 00:40:23 +0200

    Make Top Toolbar and settings activity with initial settings

--- app/src/main/java/com/example/birthdates/MainActivity.java
+++ app/src/main/java/com/example/birthdates/MainActivity.java
@@ -1,42 +1,75 @@
 package com.example.birthdates;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.birthdates.ui.SettingsActivity;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     private BottomAppBar bottomAppBar;
+    private Toolbar topToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         bottomAppBar = findViewById(R.id.bottom_app_bar);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        topToolbar = findViewById(R.id.top_toolbar);
 
          // Setting menu in the bottom app bar
         setSupportActionBar(bottomAppBar);
+        setSupportActionBar(topToolbar);
 
         initializeNavigation();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate Top Toolbar menu
+        getMenuInflater().inflate(R.menu.main_app_menu, menu);
+        // Enable divider between options groups
+        MenuCompat.setGroupDividerEnabled(menu, true);
+
+        return true;
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
 
+        switch (id) {
+            case R.id.menu_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                break;
+            case R.id.menu_login:
+                Toast.makeText(this, "Login Button pressed", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* Initialize Navigation between fragment */
     protected void initializeNavigation() {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-
-
         // Connect bottom navigation with destinations
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_people, R.id.navigation_events)
---
+++ app/src/main/java/com/example/birthdates/ui/SettingsActivity.java
@@ -0,0 +1,49 @@
+package com.example.birthdates.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.birthdates.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_frame, new SettingsFragment())
+                .commit();
+
+        // Setup toolbar
+        Toolbar toolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+//            addPreferencesFromResource(R.xml.root_preferences);
+
+        }
+    }
+}
